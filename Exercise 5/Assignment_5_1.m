ff08f4b3f0f3343714145310969ba86581d6cdaa
Nicolas Restrepo Torres <nrestrepot@unal.edu.co>
2023-10-23 19:37:09 +0300

    Comments to exercise 5

--- Exercise 5/Assignment_5_1.m
+++ Exercise 5/Assignment_5_1.m
@@ -1,14 +1,13 @@
 clc;
 
-
+%% Exercise 5 - a)
 tspan = [0 10]; % Range
 y0 = 0; % Initial point
 %% ODE solver in matlab
 [t,y] = ode45(@(t,y) -10*y + 30 - 18*exp(-t), tspan, y0);
 %% Exact solution provided
 solution = @(t) 3 - (2*exp(-t)) - 0.7*exp(-10*t);
-
-
+%% First graph: ODE45 solver vs exact solution
 figure(1);
 plot(t,y,'-o')
 hold on;
@@ -25,7 +24,7 @@ for ii = 1:numel(t)
     euclidean_distance = abs(solution(t(ii)) - y(ii));
     errors = [errors, euclidean_distance];
 end
-
+%% Second graph: Absolute error in ODE45 solver function against exact solutin
 figure(2);
 hold on;
 plot(t, errors, 'r--');
